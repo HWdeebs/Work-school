19c688d6fd7d8ba166698e5930150bf4f04743c4
HW <119452436+HWdeebs@users.noreply.github.com>
2025-02-20 12:38:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 db_connection = mysql.connector.connect(
-    host = '10.0.0.235',
-    database = 'test',
-    user = 'jay',
-    password = 'Blow66!!'
+    host = '',
+    database = '',
+    user = '',
+    password = ''
 )
 
